decd9ca238c48751e0732b081e07ea062fa7b4f0
Bart Lantz <bartlantz@gmail.com>
2024-12-02 13:19:39 -0600

    more descriptive tests

--- tests/day2.rb
+++ tests/day2.rb
@@ -3,25 +3,25 @@ require_relative "../day2/report"
 
 class TestDay2 < Minitest::Test
   def setup
-    @report = Report.new("10 9 7 5 3 1")
-    @report2 = Report.new("10 4 8 7 5")
-    @report3 = Report.new("3 10 4 8 7 5")
+    @safe_report = Report.new("10 9 7 5 3 1")
+    @one_bad_level_report = Report.new("10 4 8 7 5")
+    @multiple_bad_report = Report.new("3 10 4 8 7 5")
   end
 
-  def test_that_report_is_safe
-    assert_equal true, @report.safe?
+  def test_that_safe_report_is_safe
+    assert_equal true, @safe_report.safe?
   end
 
   def test_that_one_bad_level_is_marked_unsafe
-    refute_match true, @report2.safe?
+    refute_match true, @one_bad_level_report.safe?
   end
 
   def test_that_one_bad_level_will_be_ignored_with_dampener
-    assert_equal true, @report2.any_safe?
+    assert_equal true, @one_bad_level_report.any_safe?
   end
 
   def test_that_multiple_bad_level_will_be_not_be_ignored_with_dampener
-    refute_match true, @report3.any_safe?
+    refute_match true, @multiple_bad_report.any_safe?
   end
 
   def test_that_will_be_skipped
