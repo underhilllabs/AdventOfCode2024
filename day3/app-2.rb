935e37769ea0acc811135dfbd53215ec728c2fd8
Bart Lantz <underhilllabs@gmail.com>
2024-12-03 16:47:24 -0600

    removes debug puts statements

--- day3/app-2.rb
+++ day3/app-2.rb
@@ -4,7 +4,6 @@ FILE="input.txt"
 DEBUG = false
 
 data = File.open(FILE).readlines
-puts "starting data: #{data}" if DEBUG
 
 process_data = data.join
 
@@ -12,13 +11,10 @@ re = /mul\(\d+,\d+\)|don\'t\(\)|do\(\)/
 nums = []
 flagged = false
 process_data.scan(re).each do |str|
-  puts "current str #{str}" if DEBUG
   flagged = true if str == "don't()"
   flagged = false if str == "do()"
-  puts "skipping because in don't() loop" if flagged == true and DEBUG
   next if flagged
   mults = str.scan(/\d+/)
-  puts "found #{mults}" if DEBUG
   nums.append(mults[0].to_i * mults[1].to_i) if mults.respond_to?('each')
 end
 
