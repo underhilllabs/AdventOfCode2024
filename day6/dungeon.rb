23d0c522c2d51dd41f49cf975cd31bd0c3f21085
Bart Lantz <underhilllabs@gmail.com>
2024-12-06 07:32:01 -0700

    day 6 part 1, start

---
+++ day6/dungeon.rb
@@ -0,0 +1,19 @@
+class Dungeon
+  attr_reader :room, :direction, :dir_x, :dir_y
+  def initialize(file)
+  end
+
+  def step
+  end
+
+  def move(x, y, dir)
+  end
+
+  def turn_right
+  end
+
+  def look(x, y, dir)
+
+  end
+end
+
